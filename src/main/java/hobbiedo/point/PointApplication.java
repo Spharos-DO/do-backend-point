4441d8e2c53642960b6d411bdf7a10bb061bf58b
Kim seonuk <kswms403@naver.com>
2024-05-30 20:08:21 +0900

    init: 포인트 서비스 프로젝트 생성 및 초기 설정

---
+++ src/main/java/hobbiedo/point/PointApplication.java
@@ -0,0 +1,14 @@
+package hobbiedo.point;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PointApplication.class, args);
+	}
+}
